963bec7d64589c5bf2cab5d9047443224c80ad46
Eesha <eeshaskaul@gmail.com>
2019-07-01 16:43:24 -0400

    Fixed directory to include links (they do not work) and included more stylistic elements (includes table and file count)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <style>
 body {
-  background-color: #E6E6E6;
+  background-color: lightblue;
 }
 
 h1 {
@@ -24,19 +24,40 @@ p {
 
 <?php 
 
-// Changes ~esk2 to home/esk2/public_html
-$requestedpath = $_SERVER["REQUEST_URI"];
-$realRequestedPath = str_replace("~esk2", "home/esk2/public_html", $requestedpath);
+// Open this directory 
+$myDirectory = opendir(".");
 
-// Displays the file and folder names  
-$dir = new DirectoryIterator($realRequestedPath);
-foreach ($dir as $fileinfo) {
-    if (!$fileinfo->isDot()) {
-        var_dump($fileinfo->getFilename());
+// Get each entry
+while($entryName = readdir($myDirectory)) {
+    $dirArray[] = $entryName;
+}
+
+// Close directory
+closedir($myDirectory);
+
+//  Count elements in array
+$indexCount = count($dirArray);
+Print ("$indexCount files<br>\n");
+
+// Sort them
+sort($dirArray);
+
+// Print them
+print("<TABLE border=1 cellpadding=5 cellspacing=0 class=whitelinks>\n");
+print("<TR><TH>Filename</TH><th>Filetype</th><th>Filesize</th></TR>\n");
+
+// Loop through the array of files and print them all
+for($index=0; $index < $indexCount; $index++) {
+        if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
+        print("<TR><TD><a href=\"$dirArray[$index]\">$dirArray[$index]</a></td>");
+        print("<td>");  print(filetype($dirArray[$index])); print("</td>");
+        print("<td>");  print(filesize($dirArray[$index])); print("</td>");
+        print("</TR>\n");
     }
 }
+print("</TABLE>\n");
 
 ?> 
 
 </body> 
-</html> 
+</html>
