37ef00308fb9859a344ec54ca8991210e00112c8
Eesha <eeshaskaul@gmail.com>
2019-07-23 12:00:29 -0400

    Added 'Last Modified' code and 'Download All' button code, both need to be debugged/fixed slightly

--- index.php
+++ index.php
@@ -12,17 +12,15 @@
 
 </head> 
 
-<!-- Download All button & function -->
+<!--  Download All button & function/ This needs to be fixed, it doesn't work properly    -->
 
 <button id="download">Download</button> 
 
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
   <script type="text/javascript">
-
      $('#download').click(function() {
        download('http://nogin.info/cv.doc','http://nogin.info/cv.doc');
      });
-
      var download = function() {
        for(var i=0; i<arguments.length; i++) {
          var iframe = $('<iframe style="visibility: collapse;"></iframe>');
@@ -38,9 +36,7 @@
          })(iframe), 2000);
        }
      }      
-
   </script>
-
 
 
 <?php 
@@ -68,23 +64,45 @@ Print ("$indexCount files<br>\n");
 // Sort them
 sort($dirArray);
 
-// Print them
+// Print them 
 print("<TABLE border=1 cellpadding=5 cellspacing=0 class=whitelinks>\n");
-print("<TR><TH>Filename</TH> <th>Filetype</th> <th>Filesize</th> </TR>\n");
+print("<TR><TH>File Name</TH> <th>File Type</th> <th>File Size</th> <th>Last Modified</th> </TR>\n");
 
 
 // Loop through the array of files and print them all
 for($index=0; $index < $indexCount; $index++) {
         if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
-          print("<TR><TD><a href=\"$dirArray[$index]\">$dirArray[$index]</a></td>");
+            print("<TR><TD
+            class=\"filename\"><a href=\"$dirArray[$index]\">$dirArray[$index]</a></td>");
           print("<td>");  print(filetype($realRequestedPath . DIRECTORY_SEPARATOR. $dirArray[$index])); print("</td>");
-          print("<td>");  print(filesize($realRequestedPath . DIRECTORY_SEPARATOR. $dirArray[$index]) . " B" ); print("</td>");
+          print("<td>");  print(filesize($realRequestedPath . DIRECTORY_SEPARATOR. $dirArray[$index]) . " B" ); print("</td>"); 
           print("</TR>\n");
     }
 }
 print("</TABLE>\n");
 
+
+// "Last modified" code , new code than at 11:30 9saved as a draft in your email) but displays: 'This file was last modified on: 07/23/2019" with updated time  
+// Shows up at bottom up table/above footer and for each directory/file, not for each file which is the intended goal 
+
+$file = $_SERVER["SCRIPT_NAME"];
+    $break = Explode('/', $file);
+    $pfile = $break[count($break) - 1]; 
+//echo $pfile;
+echo "This file was last modified on: " .date("m/d/Y H:i:s A ",filemtime($pfile));
+
+
 ?> 
+
+
+<!-- Footer --> 
+<br/>
+<hr/>
+<div align="center">
+   <a href="http://www.nist.gov/" class="eplan">NIST</a> is an agency of the <a href="http://www.commerce.gov/">U.S. Department of Commerce.</a>
+</div>
+</div>
+</footer>
 
 </body> 
 </html>
